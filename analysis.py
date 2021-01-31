b9d73410ec105f707d4f6bc93ba3e73776a7b7f6
Gabri <gabripg90@gmail.com>
2021-01-31 14:06:49 +0100

    to review for v3

--- analysis.py
+++ analysis.py
@@ -1,10 +1,17 @@
-from utils import get_stock_fundamentals, filter_stocks
+from utils import get_basic_stock_fundamentals, filter_stocks, stock_data_builder, assets_info
+
 if __name__ == '__main__':
+    # Basic fundamentals for a list of stocks
     stock_list = ['aapl', 'AMZN']
+    stocks_df = get_basic_stock_fundamentals(stock_list)
 
-    stocks_df = get_stock_fundamentals(stock_list)
-
+    # Filter stock values by ratios filter
     ratios_filter = {'roce': 0.2, 'ev_fcf': 45, 'momentum': 1, 'beta': 2}
-
     stocks_filtered = filter_stocks(ratios_filter, stocks_df)
 
+    # Get the fundamentals for one or more stock values
+    all_fundamentals = stock_data_builder(['aapl'])
+    df = stock_data_builder(['aapl', 'AMZN', 'apc.f'])
+
+    # Get assets info
+    assets_info = assets_info(stock_list)
--- full_stock_data.py
+++
@@ -1,4 +0,0 @@
-from stock_class import STOCK
-from stock_data_builder_class import stock_data_builder
-if __name__ == '__main__':
-    price = stock_data_builder('aapl')
